fbee781cc59fbdf0273715b5648e8c3edbdfa534
daishixiang <809537981@qq.com>
2019-12-06 17:07:20 +0800

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,62 @@ export function sort<T extends Dictionary<any>, K extends keyof T>(arry: T[], ke
   }
   return arry.sort(compare(key));
 }
+
+/**
+ * 判断字符串是不是由数字组成
+ * @param str
+ */
+export function isNumberString(str: string) {
+  return /^\d+$/.test(str);
+}
+
+/**
+ * 最多两位小数点金钱
+ * @param str
+ */
+export function isMoney(str: string) {
+  return /^(0|[1-9]\d*)(\.\d{1,2})?$/.test(str);
+}
+
+/**
+ * 汉字转Unicode编码
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ */
+export function toUnicode(str: string) {
+  return `\\u${str.charCodeAt(0).toString(16)}`;
+}
+
+/**
+ * 绝对路径转相对路径
+ *
+ * @export
+ * @param {string} path
+ */
+export function pathToRelative(path: string) {
+  return path.replace(/https?:\/{2}[^\/]+/, '.');
+}
+
+/**
+ * 检测用户名
+ * 匹配规则：只能是中文，英文，数字，下划线，4-16个字符；
+ *
+ * @export
+ * @param {string} name
+ */
+export function isSecurityName(name: string) {
+  return /^[\u4E00-\u9FA5\uf900-\ufa2d\w]{4,16}$/.test(name);
+}
+
+/**
+ * 检测身份证号码
+ * 匹配规则：身份证号码有15位或者18位，其中最后一位可能是X，其他全是数字
+ *
+ * @export
+ * @param {string} id
+ */
+export function isIdNumber(id: string) {
+  return /^\d{14}|\d{17}(\d|[xX])$/.test(id);
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -83,4 +83,84 @@ describe('tools.js', () => {
       ]);
     });
   });
+
+  describe('tools.isNumberString', () => {
+    test('tools.isNumberString should return true', () => {
+      expect(tools.isNumberString('01234')).toBeTruthy();
+    });
+
+    test('tools.isNumberString(" 01234") should return false', () => {
+      expect(tools.isNumberString(' 01234')).toBeFalsy();
+    });
+
+    test('tools.isNumberString("01234") should return false', () => {
+      expect(tools.isNumberString('01234 ')).toBeFalsy();
+    });
+
+    test('tools.isNumberString("012 34") should return false', () => {
+      expect(tools.isNumberString('012 34')).toBeFalsy();
+    });
+  });
+
+  describe('tools.isMoney', () => {
+    test('tools.isMoney("1234.11") should return true', () => {
+      expect(tools.isMoney('1234.11')).toBeTruthy();
+    });
+
+    test('tools.isMoney(" 01234") should return false', () => {
+      expect(tools.isMoney('01234.1')).toBeFalsy();
+    });
+
+    test('tools.isNumberString("1234.000") should return false', () => {
+      expect(tools.isMoney('1234.000')).toBeFalsy();
+    });
+
+    test('tools.isMoney("0.11") should return true', () => {
+      expect(tools.isMoney('0.11')).toBeTruthy();
+    });
+  });
+
+  describe('tools.toUnicode', () => {
+    test('tools.toUnicode("我") should return "\\u6211"', () => {
+      expect(tools.toUnicode('我')).toBe('\\u6211');
+    });
+  });
+
+  describe('tools.pathToRelative', () => {
+    test('tools.pathToRelative("http://172.16.28.162/images/a.jpg") should return "./images/a.jpg"', () => {
+      expect(tools.pathToRelative('http://172.16.28.162/images/a.jpg')).toBe('./images/a.jpg');
+    });
+
+    test('tools.pathToRelative("https://www.w3school.com.cn/jsref/index.asp") should return "./images/a.jpg"', () => {
+      expect(tools.pathToRelative('https://www.w3school.com.cn/jsref/index.asp')).toBe('./jsref/index.asp');
+    });
+  });
+
+  describe('tools.isSecurityName', () => {
+    test('tools.isSecurityName("http://1") should return false', () => {
+      expect(tools.isSecurityName('http://1')).toBeFalsy();
+    });
+
+    test('tools.isSecurityName("index.asp") should return "false"', () => {
+      expect(tools.isSecurityName('index.asp')).toBeFalsy();
+    });
+
+    test('tools.isSecurityName("Edward_我的789798") should return "true"', () => {
+      expect(tools.isSecurityName('Edward_我的789798')).toBeTruthy();
+    });
+  });
+
+  describe('tools.isIdNumber', () => {
+    test('tools.isIdNumber("411527199605300414") should return true', () => {
+      expect(tools.isIdNumber('411527199605300414')).toBeTruthy();
+    });
+
+    test('tools.isIdNumber("41152719960530041x") should return true', () => {
+      expect(tools.isIdNumber('41152719960530041x')).toBeTruthy();
+    });
+
+    test('tools.isIdNumber("411527199605300") should return true', () => {
+      expect(tools.isIdNumber('411527199605300')).toBeTruthy();
+    });
+  });
 });
