3077d29cb0b63d3f40a840b2426bde8f91b2f2eb
daishixiang <809537981@qq.com>
2019-12-30 17:36:50 +0800

    feat: 添加www.w3cschool.cn 的初级算法题

---
+++ src/algorithm.ts
@@ -0,0 +1,204 @@
+/**
+ * 翻转字符串
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ */
+export function reverseString(str: string) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
+
+/**
+ * 阶乘
+ *
+ * @export
+ * @param {number} num
+ * @returns
+ * @example factorialize(5) === 120
+ */
+export function factorialize(num: number) {
+  let i = 1;
+  const arr: number[] = [];
+  while (i <= num) {
+    arr.push(i);
+    i++;
+  }
+
+  return arr.reduce((previousValue, currentValue) => {
+    return previousValue * currentValue;
+  }, 1);
+}
+
+/**
+ * 判断是不是回文字符串
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ * @example palindrome("racecar") === true
+ */
+export function palindrome(str: string) {
+  const result = str.replace(/[\s\,\.]+/g, '').toLowerCase();
+  return (
+    result ===
+    result
+      .split('')
+      .reverse()
+      .join('')
+  );
+}
+
+/**
+ * 找出最长的字符串的长度
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ */
+export function findLongestWord(str: string) {
+  return str.split(/\s/).sort((a, b) => b.length - a.length)[0].length;
+}
+
+/**
+ * 首字母大写
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ */
+export function titleCase(str: string) {
+  return str
+    .split(/\s/)
+    .map(it => it.replace(/([a-z])([a-z]*)/i, (m, a, b) => a.toUpperCase() + b.toLowerCase()))
+    .join(' ');
+}
+
+/**
+ * 寻找数组中的最大值
+ *
+ * @export
+ * @param {number[][]} arr
+ * @returns
+ */
+export function largestOfFour(arr: number[][]) {
+  return arr.map(item => Math.max.apply(null, item));
+}
+
+export function confirmEnding(str: string, target: string) {
+  return str.slice(-target.length) === target;
+}
+
+export function confirmEndingRrgExp(str: string, target: string) {
+  const reg = new RegExp(target + '$');
+  return reg.test(str);
+}
+
+/**
+ * 重复字符串
+ *
+ * @export
+ * @param {string} str
+ * @param {number} repeat
+ * @returns
+ * @example repeatString("*",3) => "***"
+ */
+export function repeatString(str: string, repeat: number) {
+  return new Array(Math.max(0, repeat)).fill(str).join('');
+}
+
+/**
+ * 字符串截取算
+ *
+ * @export
+ * @param {string} str
+ * @param {number} num
+ * @returns
+ */
+export function truncate(str: string, num: number) {
+  if (str.length > num && num > 3) {
+    return str.slice(0, num - 3).concat('...');
+  } else if (str.length > num && num <= 3) {
+    return str.slice(0, num).concat('...');
+  } else if (str.length <= num) {
+    return str;
+  }
+}
+
+/**
+ * 根据 size 分割数组
+ *
+ * @export
+ * @param {any[]} arr
+ * @param {number} size
+ * @returns
+ */
+export function chunk<T>(arr: T[], size: number) {
+  const result: T[][] = [[]];
+  arr.forEach((item, i) => {
+    if ((i + 1) % size === 0) {
+      result[result.length - 1].push(item);
+      if (i !== arr.length - 1) result.push([]);
+    } else {
+      result[result.length - 1].push(item);
+    }
+  });
+  return result;
+}
+
+export function bouncer(arr: number[], ...arg: number[]) {
+  arr.filter(item => arg.indexOf(item) > -1);
+}
+
+/**
+ * 位移密码算法
+ *
+ * @export
+ * @param {string} str
+ * @returns
+ */
+export function rot13(str: string) {
+  const offset = 13;
+  const A = 'A'.charCodeAt(0);
+  const Z = 'Z'.charCodeAt(0);
+  const strCode: number[] = str.split('').map(it => it.charCodeAt(0));
+  return strCode
+    .map(code => {
+      if (code >= A && code <= Z) {
+        return code - offset < A
+          ? String.fromCharCode(Z - (A - (code - offset + 1)))
+          : String.fromCharCode(code - offset);
+      }
+      return String.fromCharCode(code);
+    })
+    .join('');
+}
+
+/**
+ * 区间求值
+ *
+ * @export
+ * @param {number[]} arr
+ * @returns
+ */
+export function sumAll(arr: number[]) {
+  let min = Math.min.apply(null, arr);
+  const max = Math.max.apply(null, arr);
+  const result: number[] = [];
+  while (min <= max) {
+    result.push(min);
+    min++;
+  }
+  return result.reduce((pre, curr) => pre + curr);
+}
+
+export function diff(arr1: number[], arr2: number[]) {
+  const result: number[] = [];
+  arr1.forEach(item => {
+    if (arr2.includes(item)) result.push(item);
+  });
+  return arr1.filter(item => !result.includes(item)).concat(arr2.filter(item => !result.includes(item)));
+}
---
+++ test/algoritgm.spec.ts
@@ -0,0 +1,108 @@
+import * as algorithm from '../src/algorithm';
+
+describe('algorithm', () => {
+  describe('algorithm.palindrome', () => {
+    test('algorithm.palindrome("The quick dog") should return false', () => {
+      expect(algorithm.palindrome('The quick dog')).toBeFalsy();
+    });
+
+    test('algorithm.palindrome("e ye") should return true', () => {
+      expect(algorithm.palindrome('e ye')).toBeTruthy();
+    });
+  });
+
+  test('algorithm.reverseString("e ye") should return true', () => {
+    expect(algorithm.reverseString('123')).toBe('321');
+  });
+
+  test('algorithm.factorialize(5) should return true', () => {
+    expect(algorithm.factorialize(5)).toBe(120);
+  });
+
+  test('algorithm.findLongestWord("The quick brown fox jumped over the lazy dog") should return 6', () => {
+    expect(algorithm.findLongestWord('The quick brown fox jumped over the lazy dog')).toBe(6);
+  });
+
+  test('algorithm.findLongestWord("The quick brown") should return "The Quick Brown"', () => {
+    expect(algorithm.titleCase('The quick brown')).toBe('The Quick Brown');
+  });
+
+  describe('algorithm.largestOfFour', () => {
+    test('largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]])  应该返回一个数组 ', () => {
+      expect(
+        algorithm.largestOfFour([
+          [4, 5, 1, 3],
+          [13, 27, 18, 26],
+          [32, 35, 37, 39],
+          [1000, 1001, 857, 1],
+        ]),
+      ).toBeInstanceOf(Array);
+    });
+
+    test('largestOfFour([[13,27,18,26],[4,5,1,3],[32,35,37,39],[1000,1001,857,1]]) 应该返回 [27,5,39,1001]', () => {
+      expect(
+        algorithm.largestOfFour([
+          [13, 27, 18, 26],
+          [4, 5, 1, 3],
+          [32, 35, 37, 39],
+          [1000, 1001, 857, 1],
+        ]),
+      ).toEqual([27, 5, 39, 1001]);
+    });
+  });
+
+  describe('algorithm.truncate', () => {
+    test('truncate("A-tisket a-tasket A green and yellow basket", 11)', () => {
+      expect(algorithm.truncate('A-tisket a-tasket A green and yellow basket', 11)).toBe('A-tisket...');
+    });
+
+    test('truncate("Peter Piper picked a peck of pickled peppers", 14)', () => {
+      expect(algorithm.truncate('Peter Piper picked a peck of pickled peppers', 14)).toBe('Peter Piper...');
+    });
+
+    test('truncate("A-", 1)', () => {
+      expect(algorithm.truncate('A-', 1)).toBe('A...');
+    });
+
+    test('truncate("Absolutely Longer", 2)', () => {
+      expect(algorithm.truncate('Absolutely Longer', 2)).toBe('Ab...');
+    });
+
+    test('truncate("Longer", 6)', () => {
+      expect(algorithm.truncate('Longer', 6)).toBe('Longer');
+    });
+  });
+
+  describe('algorithm.repeatString', () => {
+    test('repeatString("A-tisket a-tasket A green and yellow basket", 11)', () => {
+      expect(algorithm.repeatString('*', 3)).toBe('***');
+    });
+
+    test('repeatString("Absolutely Longer", 2)', () => {
+      expect(algorithm.repeatString('1', 0)).toBe('');
+    });
+  });
+
+  describe('algorithm.chunk', () => {
+    test('chunk(["a", "b", "c", "d"], 2)', () => {
+      expect(algorithm.chunk(['a', 'b', 'c', 'd'], 2)).toEqual([
+        ['a', 'b'],
+        ['c', 'd'],
+      ]);
+    });
+
+    test('chunk(["a", "b", "c", "d", "e"], 2)', () => {
+      expect(algorithm.chunk(['a', 'b', 'c', 'd', 'e'], 2)).toEqual([['a', 'b'], ['c', 'd'], ['e']]);
+    });
+  });
+
+  describe('algorithm.rot13', () => {
+    test('rot13("SERR PBQR PNZC") 应该返回 "FREE CODE CAMP"', () => {
+      expect(algorithm.rot13('SERR PBQR PNZC')).toBe('FREE CODE CAMP');
+    });
+
+    test('rot13("SERR CVMMN!") 应该返回 "FREE CODE CAMP"', () => {
+      expect(algorithm.rot13('SERR CVMMN!')).toBe('FREE PIZZA!');
+    });
+  });
+});
